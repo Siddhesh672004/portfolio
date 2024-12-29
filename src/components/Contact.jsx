78f8802db395304fa0986cf90ca3b933ba54f3bc
Siddhesh Chaudhari <siddheshavic67@gmail.com>
2024-12-29 14:28:34 +0530

    Email Functionality Implemented

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,13 +9,61 @@ import { slideIn } from "../utils/motion";
 
 const Contact = () => {
   const formRef = useRef();
-  const [form, setForm] = useState(false);
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
 
   const [loading, setLoading] = useState(false);
+  
+  const handleChange = (e) => {
+    const { target } = e;
+    const { name, value } = target;
 
-  const handleChange = (e) => {};
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
 
-  const handleSubmit = (e) => {};
+ 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .send(
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        {
+          from_name: form.name,
+          to_name: "Siddhesh",
+          from_email: form.email,
+          to_email: "siddheshavic67@gmail.com",
+          message: form.message,
+        },
+        import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert("Thank you. I will get back to you as soon as possible.");
+
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.log(error);
+
+          alert("Ahh, something went wrong. Please try again.");
+        }
+      );
+  };
 
   return (
     <div
