0e11c5e7373ea515f96f8bbb7d37141eb0caa7df
Siddhesh Chaudhari <siddheshavic67@gmail.com>
2024-12-30 10:03:34 +0530

    Hero Section Updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,48 +2,105 @@ import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
+import profileImage from "../assets/Myself.jpg";
+import backgroundImage from "../assets/background.jpg";
+import { SectionWrapper } from "../hoc";
 
 const Hero = () => {
   return (
-    <section className="relative w-full h-screen mx-auto">
-      <div
-        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
-      >
-        <div className="flex flex-col justify-center items-center mt-5">
-          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
-          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+    // <section className="relative w-full h-screen mx-auto">
+    // <div
+    //   className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+    // >
+    //   <div className="flex flex-col justify-center items-center mt-5">
+    //     <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+    //     <div className="w-1 sm:h-80 h-40 violet-gradient" />
+    //   </div>
+    //   <div>
+    //     <h1 className={`${styles.heroHeadText} text-white`}>
+    //       Hi, I'm <span className="text-[#915eff]">Siddhesh</span>
+    //     </h1>
+    //     <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+    //     I deveop Dynamic Web Applications
+    //       <br className="sm:block hidden" /> with Modern JavaScript technologies.
+    //     </p>
+    //   </div>
+    // </div>
+
+    //   <ComputersCanvas />
+
+    //   <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+    //     <a href="#about">
+    //       <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+    //         <motion.div
+    //           animate={{
+    //             y: [0, 24,0]
+    //           }}
+    //           transition={{
+    //             duration: 1.5,
+    //             repeat: Infinity,
+    //             repeatType: 'loop'
+    //           }}
+    //           className="w-3 h-3 rounded-full bg-secondary mb-1"
+    //         />
+    //       </div>
+    //     </a>
+    //   </div>
+    // </section>
+    <>
+      <div className="flex h-screen ">
+        {/* Left Side */}
+        <div className="flex-1 p-4">
+          <div
+            className={`${styles.paddingX} absolute inset-0 top-[90px] max-w-7xl mx-auto flex flex-row items-start gap-5 p-10`}
+          >
+            <div className="flex flex-col justify-center items-center mt-5">
+              <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+              <div className="w-1 sm:h-80 h-40 violet-gradient" />
+            </div>
+            <div>
+              <h1 className={`${styles.heroHeadText} text-white`}>
+                Hello, <br />
+                <span className="text-[#915eff]">I'm Siddhesh</span>
+              </h1>
+              <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+              I design and deveop experiences that make  
+                <br className="sm:block hidden" />  make people's lives simpler through Web apps. <br/>I work with HTML5, CSS3, JavaScript, and React.
+              </p>
+            </div>
+          </div>
         </div>
-        <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#915eff]">Siddhesh</span>
-          </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-          I deveop Dynamic Web Applications 
-            <br className="sm:block hidden" /> with Modern JavaScript technologies.
-          </p>
+
+        {/* Right Side */}
+        <div className="flex-1 flex justify-center items-center h-full p-4">
+          <img
+            src={profileImage}
+            alt="My Image"
+            className="w-80 h-80 rounded-full border-4 border-[#915eff]"
+          />
         </div>
       </div>
 
-      <ComputersCanvas />
-
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+      
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+           
             <motion.div
               animate={{
-                y: [0, 24,0]
+                y: [0, 24, 0],
               }}
               transition={{
                 duration: 1.5,
                 repeat: Infinity,
-                repeatType: 'loop'
+                repeatType: "loop",
               }}
               className="w-3 h-3 rounded-full bg-secondary mb-1"
             />
           </div>
         </a>
-      </div>  
-    </section>
+      </div>
+    </>
   );
 };
 
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -9,20 +9,16 @@ const Computers = ({ isMobile }) => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
-      <spotLight
-        position={[-20, 50, 10]}
-        angle={0.12}
-        penumbra={1}
-        intensity={1}
+      <ambientLight intensity={0.5} />
+      <directionalLight
+        position={[0, 10, 10]}
+        intensity={isMobile ? 0.6 : 1}
         castShadow
-        shadow-mapSize={1024}
       />
-      <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        scale={isMobile ? 0.6 : 0.75}
+        position={isMobile ? [0, -2.5, -1.8] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -55,11 +51,11 @@ const ComputersCanvas = () => {
 
   return (
     <Canvas
-      frameloop='demand'
+      frameloop="demand"
       shadows
-      dpr={[1, 2]}
-      camera={{ position: [20, 3, 5], fov: 25 }}
-      gl={{ preserveDrawingBuffer: true }}
+      dpr={[1, isMobile ? 1.5 : 2]}
+      camera={{ position: [20, 3, 5], fov: 30 }}
+      gl={{ antialias: isMobile ? false : true, preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
